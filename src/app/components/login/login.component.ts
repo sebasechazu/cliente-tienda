9cca035d35e88c57a50fadd3646a9c0b785762a4
Sebas Echazú <sebastianechazu@outlook.com>
2024-10-10 17:25:43 -0300

    primer commit

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+  email= input<string>();
+  password= input<string>();
+
+}
